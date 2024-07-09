2a0d4116529791f3bdf134225ae780d197ab8032
Ibrahim AL_Mahmoued <ibrahim.amude.781@gmail.com>
2024-07-09 18:20:37 +0300

    Some edits

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('login', [FacebookSocialiteController::class, 'handleCallback']);
+Route::post('login', [FacebookSocialiteController::class, 'handleFacebookCallback']);
